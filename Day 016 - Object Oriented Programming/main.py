d8236fa8a7d9eb6dc975fd8338256fc1e86e4896
RamenJunkie <josh@lameazoid.com>
2022-09-22 16:28:35 -0500

    Day 16

--- Day 016 - Object Oriented Programming/main.py
+++ Day 016 - Object Oriented Programming/main.py
@@ -6,14 +6,21 @@ machine = CoffeeMaker()
 drinkmenu = Menu()
 drinkitem = MenuItem
 money = MoneyMachine()
+is_on = True
 
-choice = input(f"What would you like to order? {drinkmenu.get_items()} ")
+while(is_on):
+    choice = input(f"What would you like to order? {drinkmenu.get_items()} ")
 
-if(choice == "report"):
-    machine.report()
-    money.report()
-else:
-    drink = drinkmenu.find_drink(choice)
-    if(machine.is_resource_sufficient(drink)):
-        if(money.make_payment(drink.cost)):
-            machine.make_coffee(drink)
+    if(choice == "report"):
+        machine.report()
+        money.report()
+    elif(choice == "off"):
+        print("Shutting down...  Printing final report...")
+        machine.report()
+        money.report()
+        is_on = False
+    else:
+        drink = drinkmenu.find_drink(choice)
+        if(machine.is_resource_sufficient(drink)):
+            if(money.make_payment(drink.cost)):
+                machine.make_coffee(drink)
